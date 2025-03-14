08262f6a0dddc5e1e17e9c38a161c5867b976c08
Devedse <davydavidse@gmail.com>
2025-03-14 17:16:24 +0100

    Ye

---
+++ DeveSpotnet.Db/DbModels/DbSpotHeader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveSpotnet.Db.DbModels
+{
+    public class DbSpotHeader
+    {
+        [Key]
+        public int ArticleNumber { get; set; }
+
+        public string? Subject { get; set; }
+
+        public string? From { get; set; }
+
+        public string? Date { get; set; }
+
+        public string? MessageID { get; set; }
+
+        public string? References { get; set; }
+
+        public int Bytes { get; set; }
+
+        public int Lines { get; set; }
+
+        public int Code { get; private set; }
+
+        public string? Message { get; private set; }
+    }
+}
