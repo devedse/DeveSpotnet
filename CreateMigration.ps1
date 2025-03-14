e5f97269b47a6b0ab43c9603a4f7adff76452f62
Devedse <davydavidse@gmail.com>
2025-03-14 16:51:15 +0100

    Fixes

--- CreateMigration.ps1
+++ CreateMigration.ps1
@@ -6,5 +6,3 @@ $DbContext = "DeveSpotnetDbContext"
 
 dotnet ef migrations add $MigrationName --project "./Migrations/DeveSpotnet.Migrations.Postgres" --context $DbContext -- --provider PostgreSQL
 dotnet ef migrations add $MigrationName --project "./Migrations/DeveSpotnet.Migrations.Sqlite" --context $DbContext -- --provider Sqlite
-
-Write-Host "Migration '$MigrationName' added successfully to both projects."
