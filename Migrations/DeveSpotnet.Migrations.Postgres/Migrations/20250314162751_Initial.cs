e9666f26e57b8e97ffe8c58a0a9d1b939bc58a74
Devedse <davydavidse@gmail.com>
2025-03-14 17:28:10 +0100

    Implemented initial db

---
+++ Migrations/DeveSpotnet.Migrations.Postgres/Migrations/20250314162751_Initial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DeveSpotnet.Migrations.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SpotHeaders",
+                columns: table => new
+                {
+                    ArticleNumber = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Subject = table.Column<string>(type: "text", nullable: true),
+                    From = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<string>(type: "text", nullable: true),
+                    MessageID = table.Column<string>(type: "text", nullable: true),
+                    References = table.Column<string>(type: "text", nullable: true),
+                    Bytes = table.Column<int>(type: "integer", nullable: false),
+                    Lines = table.Column<int>(type: "integer", nullable: false),
+                    Code = table.Column<int>(type: "integer", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpotHeaders", x => x.ArticleNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpotHeaders");
+        }
+    }
+}
